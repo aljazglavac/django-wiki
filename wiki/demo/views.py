0aed8043176865e51a462a300dceea5bb99bbf12
aljazglavac <aljaz.glavac97@gmail.com>
2019-04-03 09:29:12 +0200

    wiki login endpoint

--- wiki/demo/views.py
+++ wiki/demo/views.py
@@ -1,3 +1,13 @@
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 from django.shortcuts import render
 
-# Create your views here.
+
+def anno_login(request):
+    try:
+        anno = User.objects.get(username="ANNO")
+    except User.DoesNotExist:
+        return redirect('/admin/')
+    auth.login(request, anno)
+    return redirect('/admin/demo/article')
