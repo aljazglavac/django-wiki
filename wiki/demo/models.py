18b3331ee9503f1c8958dd10fc47f51265b53e13
aljazglavac <aljaz.glavac97@gmail.com>
2019-04-03 08:09:11 +0200

    demo app

---
+++ wiki/demo/models.py
@@ -0,0 +1,13 @@
+from django.db import models as m
+
+
+class Writer(m.Model):
+    first_name = m.CharField('Writers first name', max_length=10)
+    last_name = m.CharField('Writers last name', max_length=20)
+
+
+class Article(m.Model):
+    title = m.CharField('Article title', max_length=30)
+    body = m.CharField('Article body text', max_length=400)
+    pub_on = m.DateTimeField('Date this article was published.')
+    pub_by = m.ForeignKey(Writer, on_delete=models.CASCADE)
