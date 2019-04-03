0bed6445448bd8ccab001f84178bd2557741251a
aljazglavac <aljaz.glavac97@gmail.com>
2019-04-03 10:30:41 +0200

    save overwrite for wiki

--- wiki/demo/admin.py
+++ wiki/demo/admin.py
@@ -1,13 +1,32 @@
+from django.contrib.auth.models import Group
+from django.forms.models import model_to_dict
 from django.contrib import admin
-from .models import Writer, Article
-
-
-@admin.register(Writer)
-class WriterAdmin(admin.ModelAdmin):
-    pass
+from .models import Article
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'body', 'pub_on', 'pub_by')
     readonly_fields = ('wiki_id',)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            user_group = Group.objects.filter(user=request.user)[0].name
+        except:
+            user_group = None
+        if user_group == 'ANNO':
+            is_wiki = obj.pk != obj.wiki_id or obj.wiki_id == None
+            if change and not is_wiki:
+                copy_of_obj = dict(model_to_dict(obj))
+                copy_of_obj['id'] = None
+                wiki = Article(**copy_of_obj)
+                wiki.save()
+            elif change and is_wiki:
+                obj.save()
+            else:
+                obj.wiki_id = None
+                obj.save()
+        else:
+            obj.save()
+            obj.wiki_id = obj.pk
+            obj.save(update_fields=["wiki_id"])
