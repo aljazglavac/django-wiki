ebd65b56758257ccdcad699cedfd67dd743cbce8
aljazglavac <aljaz.glavac97@gmail.com>
2019-04-03 10:30:13 +0200

    removed writer model

--- wiki/demo/models.py
+++ wiki/demo/models.py
@@ -1,18 +1,11 @@
 from django.db import models as m
 
 
-class Writer(m.Model):
-    first_name = m.CharField('Writers first name', max_length=10)
-    last_name = m.CharField('Writers last name', max_length=20)
-
-    def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
-
 class Article(m.Model):
     title = m.CharField('Article title', max_length=30)
     body = m.TextField('Article body text', max_length=400)
     pub_on = m.DateTimeField('Date this article was published')
-    pub_by = m.ForeignKey(Writer, on_delete=m.CASCADE, verbose_name='Writer that wrote this article')
+    pub_by = m.CharField('Writer that wrote this article', max_length=40)
     # wiki
     wiki_id = m.IntegerField(null=True)
 
