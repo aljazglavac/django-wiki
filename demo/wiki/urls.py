dc32f69b202a8d3925327aabdaf66acebf8dc8da
aljazglavac <aljaz.glavac97@gmail.com>
2019-04-08 08:39:08 +0200

    dynamic url pattern creation based on model in db

--- demo/wiki/urls.py
+++ demo/wiki/urls.py
@@ -1,6 +1,20 @@
+from django.contrib.contenttypes.models import ContentType
 from django.urls import path
 from .views import anno_login
 
 urlpatterns = [
     path('', anno_login),
 ]
+
+
+SYS_DEF = [
+    'log entry', 'permission', 'group', 'user', 'content type', 'session'
+]
+all_content = ContentType.objects.all()
+str_content = [str(c) for c in all_content if c not in SYS_DEF]
+
+extrapatterns = []
+for model in str_content:
+    extrapatterns.append(path(model, anno_login))
+
+urlpatterns.extend(extrapatterns)
