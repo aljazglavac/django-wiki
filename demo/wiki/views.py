fd9f86829ccdb53a909fc2177174be51d74deb64
aljazglavac <aljaz.glavac97@gmail.com>
2019-04-08 10:13:14 +0200

    wiki login for models and anno over all

--- demo/wiki/views.py
+++ demo/wiki/views.py
@@ -5,9 +5,16 @@ from django.contrib import auth
 
 def anno_login(request):
     try:
-        anno = User.objects.get(username="ANNO")
-    except User.DoesNotExist:
-        return redirect('/admin/login')
-    auth.login(request, anno)
-    return redirect('/admin/')
+        model = request.path.split('/')[2]
+        anno = User.objects.get(username="ANNO-"+model)
+        auth.login(request, anno)
+        return redirect('/admin/')
+    except:
+        try:
+            anno = User.objects.get(username="ANNO")
+        except User.DoesNotExist:
+            return redirect('/admin/login')
+        auth.login(request, anno)
+        return redirect('/admin/')
+    return redirect('/admin/login')
 
