c2df634cb8e69f868081f54044288688c5fcb185
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-04-12 20:04:44 +0200

    custom manager, user can now create wiki models from this model. no need for users to make wiki_id field

--- demo/wiki/models.py
+++ demo/wiki/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models as m
 
-# Create your models here.
+
+class CustomManager(m.Manager):
+    def valid(self):
+        return self.filter(pk=m.F("wiki_id"))
+
+    def invalid(self):
+        return self.filter(~m.Q(pk=m.F("wiki_id"))
+                           | m.Q(wiki_id__isnull=True))
+
+
+class WikiModel(m.Model):
+    wiki_id = m.IntegerField(null=True)
+    objects = CustomManager()
