63003752b69c70f217a0b217efb3154ea70ec41a
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-04-13 11:25:49 +0200

    disabled messages not working as they should

--- demo/wiki/messages.py
+++ demo/wiki/messages.py
@@ -2,21 +2,31 @@ from django.contrib import messages
 
 default_success_message = 'Successfully {} suggestion'
 
+
+def add_message_only_once(request, level, msg):
+    if msg not in [m.message for m in messages.get_messages(request)]:
+        messages.add_message(request, level, msg)
+
+
 def accept_message_handler(request):
-    messages.add_message(
-        request, messages.SUCCESS,default_success_message.format('accepted'))
+    pass
+    add_message_only_once(request, messages.SUCCESS,
+                         default_success_message.format('accepted'))
 
 
 def reject_message_handler(request):
-    messages.add_message(
-        request, messages.SUCCESS,default_success_message.format('rejected'))
+    pass
+    add_message_only_once(request, messages.SUCCESS,
+                         default_success_message.format('rejected'))
 
 
 def save_message_handler(request):
-    messages.add_message(
-        request, messages.SUCCESS,default_success_message.format('saved'))
+    pass
+    add_message_only_once(request, messages.SUCCESS,
+                         default_success_message.format('saved'))
 
 
 def submit_message_handler(request):
-    messages.add_message(
-        request, messages.SUCCESS,default_success_message.format('submited'))
+    pass
+    add_message_only_once(request, messages.SUCCESS,
+                         default_success_message.format('submited'))
