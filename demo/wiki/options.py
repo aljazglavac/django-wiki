9d44f766422354aed3ac0f21f480f6f07ddf32fd
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-04-14 09:47:47 +0200

    PERMMISIONS AND GROUP ADDED

--- demo/wiki/options.py
+++ demo/wiki/options.py
@@ -1,3 +1,12 @@
 SYS_DEF = [
     'log entry', 'permission', 'group', 'user', 'content type', 'session'
 ]
+
+GROUP = 'ANNO'
+
+PERMISSIONS = [
+    'add',
+    'change',
+    'view',
+    'delete'
+]
