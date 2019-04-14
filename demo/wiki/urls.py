f9b6d53deefd53dcf3f463c17008d636a7f41f69
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-04-14 09:43:55 +0200

    use of list of all models  function

--- demo/wiki/urls.py
+++ demo/wiki/urls.py
@@ -1,22 +1,17 @@
-#from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.models import ContentType
 from django.urls import path
 from .views import anno_login, get_wiki_entries
+from .support_functions import list_of_models
 
 urlpatterns = [
     path('', anno_login),
     path('entries', get_wiki_entries),
 ]
 
-SYS_DEF = [
-    'log entry', 'permission', 'group', 'user', 'content type', 'session'
-]
 try:
-    all_content = ContentType.objects.all()
-    str_content = [str(c) for c in all_content if str(c) not in SYS_DEF]
-
     extrapatterns = []
-    for model in str_content:
-        extrapatterns.append(path(model, anno_login))
+    for model in list_of_models():
+        extrapatterns.append(path(model.__name__.lower(), anno_login))
 
     urlpatterns.extend(extrapatterns)
 except:
