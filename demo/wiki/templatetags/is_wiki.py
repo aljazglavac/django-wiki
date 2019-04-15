d68912da90b553ec545e7d49349b866a4de2992d
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-04-15 18:21:35 +0200

    use of support fun

--- demo/wiki/templatetags/is_wiki.py
+++ demo/wiki/templatetags/is_wiki.py
@@ -1,18 +1,20 @@
 from django import template
 from django.contrib.contenttypes.models import ContentType
-from wiki.support_functions import is_obj_wiki
+from wiki.support_functions import is_obj_wiki, obj_model_from_name
 
 register = template.Library()
 
+
 @register.filter(name='is_wiki')
 def is_wiki(path):
+    path = path.split('/')
     try:
-        model_str = path.split('/')[-4]
-        ct = ContentType.objects.get(model=model_str)
-        model = ct.model_class()
-    except: return False
+        model_name = path[-4]
+        model = obj_model_from_name(model_name)
+    except:
+        return False
     try:
-        p_id = int(path.split('/')[-3])
+        p_id = int(path[-3])
         return is_obj_wiki(model.objects.get(pk=p_id))
     except:
         return False
