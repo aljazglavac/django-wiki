01ffcd7aca296ea5b997de2dc0dee53bec87e52d
Aljaz Glavac <aljaz.glavac@gmail.com>
2019-04-22 17:12:31 +0200

    demo app

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include # USER ADD
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wiki/', include('wiki.urls')) # USER ADD
+    path('wiki/', include('wiki.urls'))
 ]
